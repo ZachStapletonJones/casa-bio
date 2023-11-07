ce5cfe4c5c63f42985ca2c90ba62fd6e572c3c90
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2023-11-07 02:06:10 -0500

    markdown test

--- app/[theme]/page.tsx
+++ app/[theme]/page.tsx
@@ -1,6 +1,8 @@
 import { themes } from '@/data/themes'
 import Link from 'next/link'
 import React from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 export async function generateStaticParams() { 
     return themes.map((theme) => ({
@@ -14,7 +16,7 @@ export default function ThemePage({params}:{params:{theme:string}}) {
     <div className='flex flex-col gap-8 w-full h-full grow items-start justify-center'>
         <div>{themeData?.title}</div>
         <div>{themeData?.tagline}</div>
-        <div>{themeData?.summary}</div>
+        <Markdown remarkPlugins={[remarkGfm]}>{themeData?.summary}</Markdown>
         <Link className='pt-4' href={`/out`}>{`< Home`}</Link>
     </div>
     
--- data/themes.ts
+++ data/themes.ts
@@ -16,7 +16,49 @@ export const themes:ThemeData[] = [
         title: 'FOOD & AGRICULTURE',
         slug: 'food-agriculture',
         tagline: 'Food & Agriculture addresses specific opportunities for engineering biology to reduce greenhouse gas emissions from food production and waste and towards making agriculture and food systems more robust to climate change.',
-        summary: 'Agriculture and food systems are especially vulnerable to climate change. More intense and frequent droughts, floods, and heat waves have decimated agricultural output in all parts of the world, but have been especially detrimental to the Global South.1,2,3 The impacts of these extreme climate events are compounded by a growing global population, leading to an acute need to improve food security. On top of this, many current agricultural practices even contribute to climate change and instability, through the production of greenhouse gases (particularly methane), production and over-application of synthetic nitrogen fertilizer, inefficient water use, and production of waste. The food and agriculture sector must leverage engineering biology to both minimize its impact on climate change and to sustain production in the face of abiotic and biotic stressors that result from climate change. The Food & Agriculture theme focuses on engineering biology research opportunities to enable the production of food and crops with lower greenhouse gas (GHG) emissions through climate-friendly biofertilizers, sustainable production of meat and meat-alternatives, crop and soil resilience, opportunities in ‘smart agriculture’, and opportunities to limit food waste or convert it to useful products. Importantly, further advancement of engineering biology for agriculture requires ongoing stakeholder engagement between biotech researchers, legislators, consumers, and agriculture producers in order to identify tolerable risk thresholds, lower barriers to adoption, and incentivize scale-up.',
+        summary: 
+`# The Impact of Climate Change on Agriculture
+Agriculture and food systems are **especially vulnerable to climate change**. More intense and frequent droughts, floods, and heat waves have **decimated agricultural output** in all parts of the world, but have been especially detrimental to the Global South.[^1][^2][^3]
+
+## The Need for Enhanced Food Security
+
+The impacts of these extreme climate events are compounded by a *growing global population*, leading to an acute need to improve food security. 
+
+### Current Agricultural Practices and Climate Change
+
+On top of this, many current agricultural practices even contribute to climate change and instability, through:
+- The production of greenhouse gases (particularly methane)
+- Production and over-application of synthetic nitrogen fertilizer
+- Inefficient water use
+- Production of waste
+
+## Leveraging Engineering Biology in Agriculture
+
+The food and agriculture sector must leverage engineering biology to:
+1. Minimize its impact on climate change
+2. Sustain production in the face of abiotic and biotic stressors that result from climate change
+
+### Focus Areas
+
+The **Food & Agriculture** theme focuses on engineering biology research opportunities to enable:
+- Production of food and crops with lower greenhouse gas (GHG) emissions through climate-friendly biofertilizers
+- Sustainable production of meat and meat-alternatives
+- Crop and soil resilience
+- Opportunities in ‘smart agriculture’
+- Opportunities to limit food waste or convert it to useful products
+
+## Stakeholder Engagement
+
+Importantly, further advancement of engineering biology for agriculture requires ongoing stakeholder engagement between biotech researchers, legislators, consumers, and agriculture producers in order to:
+- Identify tolerable risk thresholds
+- Lower barriers to adoption
+- Incentivize scale-up
+
+For more information, please visit the [Engineering Biology Research Consortium](https://www.ebrc.org).
+
+[^1]: Reference 1
+[^2]: Reference 2
+[^3]: Reference 3`,
         
     },
     {
