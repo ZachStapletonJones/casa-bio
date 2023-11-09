4c80312703d6968d382f1293d6904c195549801c
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2023-11-09 10:01:24 -0500

    trying asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
+    assetPrefix: 'https://zachstapletonjones.github.io/casa-bio/',
 }
 
 module.exports = nextConfig
