8f1f922285b7f5f7ae854039625f6dfb9d369c5f
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2023-11-09 10:07:06 -0500

    node env for asset path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
-    assetPrefix: 'https://zachstapletonjones.github.io/casa-bio/',
+    assetPrefix: isProd ? 'https://zachstapletonjones.github.io/casa-bio' : undefined,
 }
 
 module.exports = nextConfig
