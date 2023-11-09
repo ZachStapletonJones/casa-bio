53a5dc33862cfa3e56db0239008599e483ba0fc2
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2023-11-09 10:29:30 -0500

    new deployment method

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     basePath: "/casa-bio",
-    assetPrefix: isProd ? "/casa-bio" : undefined,
+    // assetPrefix: isProd ? "/casa-bio" : undefined,
 }
 
 module.exports = nextConfig
