ea774160377d323da97a44ef1f5e041d10610ab8
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2023-11-28 11:32:44 -0500

    trying to fix base path for assets

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     basePath: isProd ? "/casa-bio" : undefined,
-    // assetPrefix: isProd ? "/casa-bio" : undefined,
+    assetPrefix: isProd ? "/casa-bio/" : undefined,
 }
 
 module.exports = nextConfig
