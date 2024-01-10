323b2ac5397b0e2efea5304d40f18389f371349a
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2024-01-10 11:59:19 -0500

    fixed next config for preview deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    output: 'export',
-    basePath: isProd ? "/casa-bio" : undefined,
-    // assetPrefix: isProd ? "/casa-bio" : undefined,
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      swcMinify: true,
+      skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig
