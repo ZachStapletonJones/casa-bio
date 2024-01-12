6e7c91dd736fce0fefd357f1c8ae8dc556c36e52
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2024-01-12 13:57:30 -0500

    Added disclaimer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ function Footer() {
     <div className='w-full flex justify-center bg-slate-100 m-0 py-3 border-t border-slate-300'>
         <div className='max-w-7xl w-full flex justify-center items-center'>
         <div className='flex flex-col items-center text-center gap-1 text-xs'>
-          <div>This material is based upon work supported by the National Science Foundation under Contract No. 49100423P0058.</div>
+          <div>CASA-Bio is based upon work supported by the U.S. National Science Foundation under Contract No. 49100423P0058.  Any opinions, findings and conclusions or recommendations expressed in this material do not necessarily reflect the views of the U.S. National Science Foundation.</div>
           <a href='https://knowinnovation.com/' target='_blank'>Made by <strong>Knowinnovation</strong></a>
         </div>
 
