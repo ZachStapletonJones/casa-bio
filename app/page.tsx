00a7d0f639c9d5220feb7fba84b76b13c9100e2a
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2024-01-30 15:08:16 -0500

    updated times on reg buttons

--- app/page.tsx
+++ app/page.tsx
@@ -53,10 +53,10 @@ the bioeconomy. Your input will be critical for moving CASA-Bio forward to advan
               <h4 className='text-primary font-semibold'>1) Select Your Best Date</h4>
                 <div className='grid grid-cols-1 gap-1 w-fit pb-2'>
     
-                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZwkfu2gqTMjHNVi26Cu8DcC42byYBnZYgzj'}><Button variant={'outline'} className='w-fit flex items-center gap-2'><CalendarIcon />February 12th, 2024 @ 7pm EST</Button></Link>
-                <Link className='w-fit ' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZEqd-uoqD4pG9CpFexx4TF32fJCCtoBT1u5'}><Button variant={'outline'} className='w-fit flex items-center gap-2'><CalendarIcon />February 15th, 2024 @ 9am EST</Button></Link>
-                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZIqf-Ctqj8pHdZBmJVxQxAsmAGM2wcoH019'}><Button variant={'outline'} className='w-fit flex items-center gap-2'> <CalendarIcon />February 21st, 2024 @ 12pm EST </Button></Link>
-                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZ0vce2spj4sGdGkfbJutOY8a3zmOr1Hj22H'}><Button variant={'outline'} className='w-fit flex items-center gap-2'> <CalendarIcon />February 22nd, 2024 @ 5pm EST</Button></Link>
+                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZwkfu2gqTMjHNVi26Cu8DcC42byYBnZYgzj'}><Button variant={'outline'} className='w-fit flex items-center gap-2'><CalendarIcon />February 12th, 2024 @ 7pm - 8:30pm EST</Button></Link>
+                <Link className='w-fit ' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZEqd-uoqD4pG9CpFexx4TF32fJCCtoBT1u5'}><Button variant={'outline'} className='w-fit flex items-center gap-2'><CalendarIcon />February 15th, 2024 @ 9am - 10:30am EST</Button></Link>
+                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZIqf-Ctqj8pHdZBmJVxQxAsmAGM2wcoH019'}><Button variant={'outline'} className='w-fit flex items-center gap-2'> <CalendarIcon />February 21st, 2024 @ 12pm - 1:30pm EST </Button></Link>
+                <Link className='w-fit' target='_blank' href={'https://knowinnovation.zoom.us/meeting/register/tZ0vce2spj4sGdGkfbJutOY8a3zmOr1Hj22H'}><Button variant={'outline'} className='w-fit flex items-center gap-2'> <CalendarIcon />February 22nd, 2024 @ 5pm - 6:30pm EST</Button></Link>
                 </div>
                 <h4 className='text-primary font-semibold'>2) Share Your Exciting Research Idea About Your Favorite Theme!</h4>
                 <div className='w-fit'>
