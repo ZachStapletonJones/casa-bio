429bf4f2c545b04ac65a64d0a08e9e2bd4cba4ef
Zach Stapleton-Jones <43554379+ZachStapletonJones@users.noreply.github.com>
2024-06-26 12:29:28 -0400

    dates for workshops - patch 1

--- data/workshop-topics.ts
+++ data/workshop-topics.ts
@@ -20,7 +20,7 @@ export const workshopTopic: WorkshopTopicObject = [
     id: "B",
     title: "Accelerated Breeding for All Economically Important Crops and Animals",
     description: "Fueled by National Agricultural Genome Initiatives, innovative breeding methods are transforming the development of economically important crops and animals, promising significant advances in agricultural productivity and sustainability. However, multiple barriers exist to the widespread implementation of such approaches. These include the need for substantial investment in advanced tools and technologies, gaps in critical infrastructure, regulatory challenges, and concerns about the long-term ecological and genetic impacts, to name a few. A forthcoming workshop will investigate some of these challenges, focusing on the urgent need to find enabling solutions to achieve specific deliverables related to accelerated breeding.",
-    dates: "August 2024"
+    dates: "August 12th-16th, 2024"
   },
   {
     //WorkshopTopic 3
@@ -34,21 +34,21 @@ export const workshopTopic: WorkshopTopicObject = [
     id: "D",
     title: "Enabling Resilient Biobased Supply Chains",
     description: "Enabling resilient biobased supply chains for energy, chemicals, materials, and foods has the potential to simultaneously address economic, technical, environmental, and societal challenges, particularly through the development of strategic future-facing product/biomass supply chains. However, several barriers exist to the widespread implementation of new solutions. These include, but are not limited to, the need for substantial investment in: (1) advanced feedstock (raw material) production systems, (2) scale-up and demonstration of logistics and conversion technology integration, (3) gaps in end-market readiness, and (4) the need for robust policy support and societal acceptance. A forthcoming workshop will investigate key challenges, focusing on the importance and urgency of devising solutions to achieve specific, near-term deliverables related to advancing biobased supply chains.",
-    dates: "August 2024"
+    dates: "August 19th-23rd, 2024"
   },
   {
     //WorkshopTopic 5
     id: "E",
     title: "Food Protein Diversification",
     description: "Diversifying our food protein sources to feed our growing population may offer significant benefits for climate and the environment while building national and regional economies and adding new jobs within these fields. While the development of multiple approaches in this arena has seen significant progress over the last 10 years, making a step change to develop new solutions and build regional innovation ecosystems may unlock the potential of this sector of the bioeconomy. Mindful advancement of innovative solutions may help improve food security and promote regional economic growth while supporting national climate goals and expanding the sources and modifications of proteins and other nutrients beyond traditional commodities to supplement the current food system. However, several barriers exist to widespread developmental progress and eventual adoption of these solutions. These include the need for advanced biotechnological research for scalable, cost-effective biomanufacturing processes and the development of infrastructure that utilizes regional strengths and resources for a blossoming bioeconomy. A forthcoming workshop will investigate some of these challenges, focusing on prioritizing the importance and urgency of each to achieve specific, near-term deliverables for advancing alternative food protein products and other novel agricultural bioproducts.",
-    dates: "August 2024"
+    dates: "August 19th-23rd, 2024"
   },
   {
     //WorkshopTopic 6
     id: "F",
     title: "Strategies to Improve Carbon and Energy Efficiency of Biomanufacturing",
     description: "Biological processes can be used directly to reduce CO2 as well as produce products more efficiently and with less waste. Realizing this vision in a cost-effective way will require strategic research and development to identify the most efficient pathways and strategies. However, several barriers exist to optimizing biological systems and fully leveraging the advantages of biomanufacturing methods over other non-biological approaches. These include, but are not limited to, the need for enhanced carbon and energy efficiency, new innovative technologies, overcoming technical challenges in scaling biomanufacturing processes, and navigating regulatory and market acceptance. A forthcoming workshop will investigate some of these challenges, focusing on the importance and urgency of addressing them to achieve specific, near-term deliverables related to advancing the bioeconomy and optimizing biological systems for carbon reduction.",
-    dates: "August 2024"
+    dates: "August 26th-30th, 2024"
   },
 
  
